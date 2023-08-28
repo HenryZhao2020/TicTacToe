11dd74d0b7cc7efdff096f1d5540700563b63349
Henry <75873192+HenryZhao2020@users.noreply.github.com>
2023-08-27 21:54:17 -0700

    Update File.cpp

--- File.cpp
+++ File.cpp
@@ -28,3 +28,14 @@ void File::write(const QString &path, const QString &text) {
     file.write(text.toUtf8());
     file.close();
 }
+
+void File::writeLines(const QString &path, const QList<QString> &list) {
+    write(path, list.join("\n"));
+}
+
+void File::append(const QString &path, const QString &text) {
+    QFile file(path);
+    file.open(QFile::Append);
+    file.write(QString(text + "\n").toUtf8());
+    file.close();
+}

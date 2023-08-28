009d214218e88893c260029e9b74a07ef5954799
Henry <75873192+HenryZhao2020@users.noreply.github.com>
2023-08-27 21:55:37 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,11 +2,27 @@
 #include "Game.h"
 #include "Attr.h"
 
+void restoreWindow() {
+    HWND hwnd = (HWND) File::readAll("WinID").toInt();
+    SendMessage(hwnd, WM_SYSCOMMAND, SC_RESTORE, 0);
+    SetForegroundWindow(hwnd); 
+    SetActiveWindow(hwnd); 
+    SetWindowPos(hwnd, HWND_TOP, 0, 0, 0, 0, SWP_SHOWWINDOW | SWP_NOMOVE | SWP_NOSIZE);
+    RedrawWindow(hwnd, NULL, 0, RDW_FRAME | RDW_INVALIDATE | RDW_ALLCHILDREN);
+}
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QDir::setCurrent(QApplication::applicationDirPath());
-    QFontDatabase::addApplicationFont("Fonts/Ubuntu_Bold.ttf");
     QDir::setCurrent("TicTacToe_Files");
+
+    RunGuard guard("TicTacToe");
+    if (!guard.tryToRun()) {
+        restoreWindow();
+        return 0;
+    }
+
+    QFontDatabase::addApplicationFont("../Fonts/Ubuntu_Bold.ttf");
 
     app.setStyle("Fusion");
     app.setStyleSheet(File::readAll("Styles.qss"));
@@ -16,5 +32,6 @@ int main(int argc, char *argv[]) {
     game.restore();
     game.show();
 
+    File::write("WinID", QString::number(game.winId()));
     return app.exec();
 }

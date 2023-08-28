d95206495fbb3ff2680b291dcbec828a3177e337
Henry <75873192+HenryZhao2020@users.noreply.github.com>
2023-08-27 21:54:05 -0700

    Update File.h

--- File.h
+++ File.h
@@ -1,14 +1,14 @@
 #ifndef FILE_H
 #define FILE_H
 
-#include <QList>
-
 class File {
 public:
     static QString readAll(const QString &path);
     static QList<QString> readLines(const QString &path);
 
     static void write(const QString &path, const QString &text);
+    static void writeLines(const QString &path, const QList<QString> &list);
+    static void append(const QString &path, const QString &text);
 };
 
 #endif

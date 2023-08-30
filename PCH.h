affac5ca4ab58d3e24e6f9bd82ed9e9545e1b09b
Henry <75873192+HenryZhao2020@users.noreply.github.com>
2023-08-29 20:23:47 -0700

    Update PCH.h

--- PCH.h
+++ PCH.h
@@ -3,24 +3,9 @@
 
 #include <Windows.h>
 
-#include <QApplication>
-#include <QBoxLayout>
-#include <QCheckBox>
-#include <QDataStream>
-#include <QDesktopServices>
-#include <QDialog>
-#include <QDir>
-#include <QFontDatabase>
-#include <QFormLayout>
-#include <QFrame>
-#include <QGridLayout>
-#include <QLabel>
-#include <QMainWindow>
-#include <QRandomGenerator>
-#include <QPushButton>
-#include <QTabWidget>
-#include <QTextEdit>
-#include <QTimer>
+#include <QtCore>
+#include <QtGui>
+#include <QtWidgets>
 
 #include "File.h"
 #include "Pixmap.h"

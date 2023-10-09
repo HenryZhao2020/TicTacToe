1ec12954a7d1f606984000bf955b2ff4700443d3
Henry Zhao <75873192+HenryZhao2020@users.noreply.github.com>
2023-10-08 17:42:08 -0700

    Committed "Main.cpp"

--- Tic Tac Toe/Main.cpp
+++ Tic Tac Toe/Main.cpp
@@ -37,6 +37,13 @@ int main(int argc, char *argv[]) {
     QObject::connect(&app, &SingleApplication::receivedMessage,
                      &app, &receivedMessage);
 
+    auto timer = new QTimer();
+    QObject::connect(timer, &QTimer::timeout, &app, [] {
+        QApplication::setStyle("Fusion");
+        game->adjustSize();
+    });
+    timer->start(1000);
+
     game = new Game();
     game->restore();
     game->show();
